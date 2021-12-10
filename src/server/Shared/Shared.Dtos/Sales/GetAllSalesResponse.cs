f15158b6b8ddb2e811aa5b7ce8c39e3b6eae0770
vladperchi <vladperchi@gmail.com>
2021-12-10 12:29:00 -0500

    Added Sales in Shared.Dtos

---
+++ src/server/Shared/Shared.Dtos/Sales/GetAllSalesResponse.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="GetAllSalesResponse.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace InmoIT.Shared.Dtos.Sales
+{
+    public record GetAllSalesResponse(Guid Id, string ReferenceNumber, DateTime TimeStamp, string CustomerName, string CustomerPhoneNumber, string CustomerEmail, decimal Tax, decimal Discount, decimal Total, bool IsPaid, string Comment, Guid PropertyId, Guid CustomerId);
+}
