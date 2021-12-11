38125db5b8b39c28b282a04770232b0347889304
vladperchi <vladperchi@gmail.com>
2021-12-11 16:29:02 -0500

    Added Swagger Filters For Remove From parameter And Replace Version Whit Exact Value In Path

---
+++ src/server/Shared/Shared.Infrastructure/Swagger/Filters/RemoveVersionParameterFilter.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="RemoveVersionParameterFilter.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace InmoIT.Shared.Infrastructure.Swagger.Filters
+{
+    public class RemoveVersionParameterFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters.Count == 0)
+            {
+                return;
+            }
+
+            var versionParameter = operation.Parameters.Single(p => p.Name == "version");
+            operation.Parameters.Remove(versionParameter);
+        }
+    }
+}
---
+++ src/server/Shared/Shared.Infrastructure/Swagger/Filters/ReplaceVersionValueInPathFilter.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ReplaceVersionValueInPathFilter.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace InmoIT.Shared.Infrastructure.Swagger.Filters
+{
+    public class ReplaceVersionValueInPathFilter : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            var paths = new OpenApiPaths();
+
+            foreach (var (key, value) in swaggerDoc.Paths)
+                paths.Add(key.Replace("v{version}", swaggerDoc.Info.Version, StringComparison.InvariantCultureIgnoreCase), value);
+
+            swaggerDoc.Paths = paths;
+        }
+    }
+}
