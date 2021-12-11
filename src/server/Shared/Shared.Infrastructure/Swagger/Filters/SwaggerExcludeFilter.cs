f9542ce639ab2d8fa204d6a45270c61257b5da25
vladperchi <vladperchi@gmail.com>
2021-12-11 16:57:03 -0500

    Removed parameter of a custom attribute of a specified type applied to a specified member

---
+++ src/server/Shared/Shared.Infrastructure/Swagger/Filters/SwaggerExcludeFilter.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="SwaggerExcludeFilter.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Reflection;
+using InmoIT.Shared.Core.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace InmoIT.Shared.Infrastructure.Swagger.Filters
+{
+    public class SwaggerExcludeFilter : IOperationFilter
+    {
+        /// <inheritdoc/>
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (context?.MethodInfo == null)
+            {
+                return;
+            }
+
+            var parameters = context.MethodInfo.GetParameters();
+            var properties = parameters.SelectMany(x => x.ParameterType.GetProperties());
+            var propertiesToRemove = properties
+                .Where(p => p.GetCustomAttribute<SwaggerExcludeAttribute>() != null && p.GetCustomAttribute<FromQueryAttribute>() != null)
+                .Select(p => p.Name)
+                .ToHashSet(StringComparer.InvariantCultureIgnoreCase);
+
+            foreach (var parameter in operation.Parameters.ToList())
+            {
+                if (propertiesToRemove.Contains(parameter.Name))
+                {
+                    operation.Parameters.Remove(parameter);
+                }
+            }
+        }
+    }
+}
