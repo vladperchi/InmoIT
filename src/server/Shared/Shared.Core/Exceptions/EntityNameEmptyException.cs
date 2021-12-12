5c47e4300f3029cfeb225835c8a0dc71b422940f
vladperchi <vladperchi@gmail.com>
2021-12-11 20:05:32 -0500

    Added File Entity Name Empty Exception

---
+++ src/server/Shared/Shared.Core/Exceptions/EntityNameEmptyException.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="EntityNameEmptyException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Shared.Core.Exceptions
+{
+    public class EntityNameEmptyException : CustomException
+    {
+        public EntityNameEmptyException(IStringLocalizer localizer)
+            : base(localizer["The entity name is empty..."], null, HttpStatusCode.NoContent)
+        {
+        }
+    }
+}
---
+++ src/server/Shared/Shared.Infrastructure/Services/EntityReferenceService.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="EntityReferenceService.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using InmoIT.Shared.Core.Entities;
+using InmoIT.Shared.Core.Exceptions;
+using InmoIT.Shared.Core.Integration.Application;
+using InmoIT.Shared.Core.Interfaces.Contexts;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Shared.Infrastructure.Services
+{
+    public class EntityReferenceService : IEntityReferenceService
+    {
+        private readonly IAppDbContext _context;
+        private readonly IStringLocalizer<EntityReferenceService> _localizer;
+
+        public EntityReferenceService(
+            IAppDbContext context,
+            IStringLocalizer<EntityReferenceService> localizer)
+        {
+            _context = context;
+            _localizer = localizer;
+        }
+
+        public async Task<string> PropertyTrackAsync(string entityName)
+        {
+            if (string.IsNullOrWhiteSpace(entityName))
+            {
+                throw new EntityNameEmptyException(_localizer);
+            }
+
+            string referenceNumber;
+            string monthYearString = DateTime.Now.ToString("MMyy");
+            var record = _context.EntityReferences.FirstOrDefault(a => a.Entity == entityName && a.MonthYearString == monthYearString);
+            if (record != null)
+            {
+                record.Increment();
+                _context.EntityReferences.Update(record);
+                referenceNumber = GenerateReferenceNumber(entityName, record.Count, monthYearString);
+            }
+            else
+            {
+                record = new EntityReference(entityName);
+                _context.EntityReferences.Add(record);
+                referenceNumber = GenerateReferenceNumber(entityName, record.Count, monthYearString);
+            }
+
+            await _context.SaveChangesAsync();
+            return referenceNumber;
+        }
+
+        private string GenerateReferenceNumber(string entity, int count, string monthYearString)
+        {
+            return $"{entity[0]}-{monthYearString}-{count.ToString().PadLeft(5, '0')}";
+        }
+    }
+}
