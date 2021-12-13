369a43f7ffc6c0a570ae15e07b07fd8077821460
vladperchi <vladperchi@gmail.com>
2021-12-13 11:18:27 -0500

    Rename File

---
+++ src/server/Shared/Shared.Dtos/Messages/MessageRequest.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="MessageRequest.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Dtos.Messages
+{
+    public class MessageRequest
+    {
+        public string Number { get; set; }
+
+        public string Message { get; set; }
+    }
+}
