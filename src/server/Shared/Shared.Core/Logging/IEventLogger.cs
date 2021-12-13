368b4376fb97f3e9af0cc1d03ddbb041500fe328
vladperchi <vladperchi@gmail.com>
2021-12-13 11:21:05 -0500

    Update File

---
+++ src/server/Shared/Shared.Core/Logging/IEventLogger.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IEventLogger.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using InmoIT.Shared.Core.Domain;
+
+namespace InmoIT.Shared.Core.Logging
+{
+    public interface IEventLogger
+    {
+        Task SaveAsync<T>(T @event, (string oldValues, string newValues) changes)
+            where T : Event;
+    }
+}
---
+++ src/server/Shared/Shared.Core/Settings/MessageSettings.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="MessageSettings.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Core.Settings
+{
+    public class MessageSettings
+    {
+        public string MessageIdentification { get; set; }
+
+        public string MessagePassword { get; set; }
+
+        public string MessageFrom { get; set; }
+
+        public bool MessageEnableVerification { get; set; }
+    }
+}
