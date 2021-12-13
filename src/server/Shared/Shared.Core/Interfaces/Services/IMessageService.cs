5d908f7856b27dfc5f646795cd40fd9f57c808ad
vladperchi <vladperchi@gmail.com>
2021-12-13 10:52:51 -0500

    Rename File

---
+++ src/server/Shared/Shared.Core/Interfaces/Services/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IMessageService.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using InmoIT.Shared.Dtos.Messages;
+
+namespace InmoIT.Shared.Core.Interfaces.Services
+{
+    public interface IMessageService
+    {
+        Task SendAsync(MessageRequest request);
+    }
+}
