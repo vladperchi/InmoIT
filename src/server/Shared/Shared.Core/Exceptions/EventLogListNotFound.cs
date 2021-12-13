44a536fef4fd30c59905465992291c966d5bcea9
vladperchi <vladperchi@gmail.com>
2021-12-13 10:40:19 -0500

    Added File Exception Customized

---
+++ src/server/Shared/Shared.Core/Exceptions/EventLogListNotFound.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="EventLogListNotFound.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Shared.Core.Exceptions
+{
+    public class EventLogListNotFound : CustomException
+    {
+        public EventLogListNotFound(IStringLocalizer localizer)
+            : base(localizer["Event logs were not found..."], null, HttpStatusCode.NotFound)
+        {
+        }
+    }
+}
