1aaa3b6a7b79996217b6254410fb5c269649a36e
vladperchi <vladperchi@gmail.com>
2021-12-13 11:30:15 -0500

    Added Service SmtpMail With MailKit And MimeKit

---
+++ src/server/Shared/Shared.Infrastructure/Services/SmtpMailService.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="SmtpMailService.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using InmoIT.Shared.Core.Interfaces.Services;
+using InmoIT.Shared.Core.Settings;
+using InmoIT.Shared.Dtos.Mails;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace InmoIT.Shared.Infrastructure.Services
+{
+    public class SmtpMailService : IMailService
+    {
+        private readonly MailSettings _settings;
+        private readonly ILogger<SmtpMailService> _logger;
+
+        public SmtpMailService(IOptions<MailSettings> settings, ILogger<SmtpMailService> logger)
+        {
+            _settings = settings.Value;
+            _logger = logger;
+        }
+
+        public async Task SendAsync(MailRequest request)
+        {
+            try
+            {
+                var email = new MimeMessage
+                {
+                    Sender = new MailboxAddress(_settings.DisplayName, request.From ?? _settings.From),
+                    Subject = request.Subject,
+                    Body = new BodyBuilder
+                    {
+                        HtmlBody = request.Body
+                    }.ToMessageBody()
+                };
+                email.To.Add(MailboxAddress.Parse(request.To));
+                using var smtp = new SmtpClient();
+                await smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(_settings.UserName, _settings.Password);
+                await smtp.SendAsync(email);
+                await smtp.DisconnectAsync(true);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+            }
+        }
+    }
+}
