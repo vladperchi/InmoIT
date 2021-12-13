aec1ec7569c09e0df03668734f80f3382afe0827
vladperchi <vladperchi@gmail.com>
2021-12-13 13:15:35 -0500

    Discovers Controllers From List Of Application Instances

---
+++ src/server/Shared/Shared.Infrastructure/Controllers/InternalControllerFeatureProvider.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="InternalControllerFeatureProvider.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace InmoIT.Shared.Infrastructure.Controllers
+{
+    internal class InternalControllerFeatureProvider : ControllerFeatureProvider
+    {
+        protected override bool IsController(TypeInfo typeInfo)
+        {
+            if (!typeInfo.IsClass)
+            {
+                return false;
+            }
+
+            if (typeInfo.IsAbstract)
+            {
+                return false;
+            }
+
+            if (typeInfo.ContainsGenericParameters)
+            {
+                return false;
+            }
+
+            if (typeInfo.IsDefined(typeof(NonControllerAttribute)))
+            {
+                return false;
+            }
+
+            return typeInfo.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase) ||
+                   typeInfo.IsDefined(typeof(ControllerAttribute));
+        }
+    }
+}
