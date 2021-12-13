5194f1152b4c5d0e76a05af79b84ca15085b81ce
vladperchi <vladperchi@gmail.com>
2021-12-13 11:55:50 -0500

    Method created for delayed jobs, shoot-and-forget continuations, re-queue, delete background jobs

---
+++ src/server/Shared/Shared.Infrastructure/Services/HangfireService.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="HangfireService.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using InmoIT.Shared.Core.Interfaces.Services;
+using Hangfire;
+
+namespace InmoIT.Shared.Infrastructure.Services
+{
+    public class HangfireService : IJobService
+    {
+        public string Enqueue(Expression<Func<Task>> methodCall)
+        {
+            return BackgroundJob.Enqueue(methodCall);
+        }
+    }
+}
