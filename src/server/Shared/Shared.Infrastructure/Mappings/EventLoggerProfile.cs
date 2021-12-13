2fe181d5f7a1c72d38cd33a3dd1ad32181704059
vladperchi <vladperchi@gmail.com>
2021-12-13 14:00:27 -0500

    Added Profile Event Logger

---
+++ src/server/Shared/Shared.Infrastructure/Mappings/EventLoggerProfile.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="EventLoggerProfile.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using AutoMapper;
+using InmoIT.Shared.Core.Logging;
+using InmoIT.Shared.Core.Mappings.Converters;
+using InmoIT.Shared.Dtos.Identity.Logging;
+
+namespace InmoIT.Shared.Infrastructure.Mappings
+{
+    public class EventLoggerProfile : Profile
+    {
+        public EventLoggerProfile()
+        {
+            CreateMap<PaginatedLogFilter, GetAllLogsRequest>()
+                .ForMember(dest => dest.OrderBy, opt => opt.ConvertUsing<string>(new OrderByConverter()));
+            CreateMap<LogRequest, EventLog>().ReverseMap();
+        }
+    }
+}
