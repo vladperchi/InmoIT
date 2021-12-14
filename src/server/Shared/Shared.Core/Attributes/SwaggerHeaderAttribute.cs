8737fdb924bac0c92a94b7bec2b8fafdc81b3ace
vladperchi <vladperchi@gmail.com>
2021-12-13 20:33:35 -0500

    Added swagger paramters passed by header

---
+++ src/server/Shared/Shared.Core/Attributes/SwaggerHeaderAttribute.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="SwaggerHeaderAttribute.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace InmoIT.Shared.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class SwaggerHeaderAttribute : Attribute
+    {
+        public string HeaderName { get; }
+
+        public string Description { get; }
+
+        public string DefaultValue { get; }
+
+        public bool IsRequired { get; }
+
+        public SwaggerHeaderAttribute(string headerName, string description = null, string defaultValue = null, bool isRequired = false)
+        {
+            HeaderName = headerName;
+            Description = description;
+            DefaultValue = defaultValue;
+            IsRequired = isRequired;
+        }
+    }
+}
