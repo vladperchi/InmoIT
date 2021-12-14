84863f36bee48a3c0f89515cf5f0f02d83850f2b
vladperchi <vladperchi@gmail.com>
2021-12-14 00:07:03 -0500

    Added CorsPolicy As Extensions

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/CorsExtensions.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="CorsExtensions.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using InmoIT.Shared.Core.Extensions;
+using InmoIT.Shared.Core.Settings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InmoIT.Shared.Infrastructure.Extensions
+{
+    public static class CorsExtensions
+    {
+        internal static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+        {
+            var corsSettings = services.GetOptions<CorsSettings>(nameof(CorsSettings));
+            return services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(corsSettings.Url);
+                });
+            });
+        }
+    }
+}
