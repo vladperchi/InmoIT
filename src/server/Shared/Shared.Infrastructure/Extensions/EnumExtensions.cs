22dd7278e6162ff1a602af57c82928da97c764de
vladperchi <vladperchi@gmail.com>
2021-12-14 11:52:53 -0500

    Added Get Description String

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="EnumExtensions.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel;
+
+namespace InmoIT.Shared.Infrastructure.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescriptionString(this Enum val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString())?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes is { Length: > 0 }
+                 ? attributes[0].Description
+                 : val.ToString();
+        }
+    }
+}
