7217de1ba8a642ad8f2e28fc2454746ba9487e03
vladperchi <vladperchi@gmail.com>
2021-12-14 19:14:52 -0500

    Added Service Upload File

---
+++ src/server/Shared/Shared.Infrastructure/Services/UploadService.cs
@@ -0,0 +1,118 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UploadService.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using InmoIT.Shared.Core.Interfaces.Services;
+using InmoIT.Shared.Dtos.Upload;
+using InmoIT.Shared.Infrastructure.Extensions;
+
+namespace InmoIT.Shared.Infrastructure.Services
+{
+     public class UploadService : IUploadService
+    {
+        public Task<string> UploadAsync(FileUploadRequest request)
+        {
+            if (request.Data == null)
+            {
+                return Task.FromResult(string.Empty);
+            }
+
+            string base64Data = Regex.Match(request.Data, "data:image/(?<type>.+?),(?<data>.+)").Groups["data"].Value;
+
+            var streamData = new MemoryStream(Convert.FromBase64String(base64Data));
+            if (streamData.Length > 0)
+            {
+                string folder = request.UploadStorageType.ToDescriptionString();
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    folder = folder.Replace(@"\", "/");
+                }
+
+                string folderName = Path.Combine("Files", folder);
+                string pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                bool exists = Directory.Exists(pathToSave);
+                if (!exists)
+                {
+                    Directory.CreateDirectory(pathToSave);
+                }
+
+                string fileName = request.FileName.Trim('"');
+                string fullPath = Path.Combine(pathToSave, fileName);
+                string dbPath = Path.Combine(folderName, fileName);
+                if (File.Exists(dbPath))
+                {
+                    dbPath = NextAvailableFilename(dbPath);
+                    fullPath = NextAvailableFilename(fullPath);
+                }
+
+                using var stream = new FileStream(fullPath, FileMode.Create);
+                streamData.CopyTo(stream);
+                return Task.FromResult(dbPath);
+            }
+            else
+            {
+                return Task.FromResult(string.Empty);
+            }
+        }
+
+        private static string numberPattern = " ({0})";
+
+        private static string NextAvailableFilename(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return path;
+            }
+
+            if (Path.HasExtension(path))
+            {
+                return GetNextFilename(path.Insert(path.LastIndexOf(Path.GetExtension(path), StringComparison.Ordinal), numberPattern));
+            }
+
+            return GetNextFilename(path + numberPattern);
+        }
+
+        private static string GetNextFilename(string pattern)
+        {
+            string temp = string.Format(pattern, 1);
+
+            if (!File.Exists(temp))
+            {
+                return temp;
+            }
+
+            int min = 1, max = 2;
+
+            while (File.Exists(string.Format(pattern, max)))
+            {
+                min = max;
+                max *= 2;
+            }
+
+            while (max != min + 1)
+            {
+                int pivot = (max + min) / 2;
+                if (File.Exists(string.Format(pattern, pivot)))
+                {
+                    min = pivot;
+                }
+                else
+                {
+                    max = pivot;
+                }
+            }
+
+            return string.Format(pattern, max);
+        }
+    }
+}
