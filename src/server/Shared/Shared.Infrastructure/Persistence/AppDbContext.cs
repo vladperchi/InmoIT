4298f4d8508f4231ea7700c26803a3cceca4b689
vladperchi <vladperchi@gmail.com>
2021-12-14 19:54:24 -0500

    Added Context Applicatiion & Module

---
+++ src/server/Shared/Shared.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="AppDbContext.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using InmoIT.Shared.Core.Entities;
+using InmoIT.Shared.Core.Logging;
+using InmoIT.Shared.Core.Interfaces.Contexts;
+using InmoIT.Shared.Core.Settings;
+using InmoIT.Shared.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace InmoIT.Shared.Infrastructure.Persistence
+{
+    internal class AppDbContext : DbContext, IAppDbContext
+    {
+        private readonly PersistenceSettings _persistenceOptions;
+
+        protected string Schema => "Application";
+
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options,
+            IOptions<PersistenceSettings> persistenceOptions)
+                : base(options)
+        {
+            _persistenceOptions = persistenceOptions.Value;
+        }
+
+        public DbSet<EventLog> EventLogs { get; set; }
+
+        public DbSet<EntityReference> EntityReferences { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema(Schema);
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyApplicationConfiguration(_persistenceOptions);
+        }
+    }
+}
---
+++ src/server/Shared/Shared.Infrastructure/Persistence/ModuleDbContext.cs
@@ -0,0 +1,125 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ModuleDbContext.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using InmoIT.Shared.Core.Domain;
+using InmoIT.Shared.Core.Logging;
+using InmoIT.Shared.Core.Settings;
+using InmoIT.Shared.Infrastructure.Extensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Options;
+using InmoIT.Shared.Core.Interfaces.Contexts;
+using InmoIT.Shared.Core.Interfaces.Serialization.Serializer;
+
+namespace InmoIT.Shared.Infrastructure.Persistence
+{
+    public abstract class ModuleDbContext : DbContext, IModuleDbContext
+    {
+        private readonly IMediator _mediator;
+        private readonly IEventLogger _eventLogger;
+        private readonly PersistenceSettings _persistenceOptions;
+        private readonly IJsonSerializer _json;
+
+        protected abstract string Schema { get; }
+
+        protected ModuleDbContext(
+            DbContextOptions options,
+            IMediator mediator,
+            IEventLogger eventLogger,
+            IOptions<PersistenceSettings> persistenceOptions,
+            IJsonSerializer json)
+                : base(options)
+        {
+            _mediator = mediator;
+            _eventLogger = eventLogger;
+            _persistenceOptions = persistenceOptions.Value;
+            _json = json;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (!string.IsNullOrWhiteSpace(Schema))
+            {
+                modelBuilder.HasDefaultSchema(Schema);
+            }
+
+            modelBuilder.Ignore<Event>();
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            modelBuilder.ApplyModuleConfiguration(_persistenceOptions);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var changes = OnBeforeSaveChanges();
+            return await this.SaveChangeWithPublishEventsAsync(_eventLogger, _mediator, changes, _json, cancellationToken);
+        }
+
+        private List<(EntityEntry entityEntry, string oldValues, string newValues)> OnBeforeSaveChanges()
+        {
+            var result = new List<(EntityEntry entityEntry, string oldValues, string newValues)>();
+            ChangeTracker.DetectChanges();
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.State == EntityState.Unchanged)
+                {
+                    continue;
+                }
+
+                var previousData = new Dictionary<string, object>();
+                var currentData = new Dictionary<string, object>();
+                foreach (var property in entry.Properties)
+                {
+                    string propertyName = property.Metadata.Name;
+                    object originalValue = entry.GetDatabaseValues()?.GetValue<object>(propertyName);
+                    switch (entry.State)
+                    {
+                        case EntityState.Unchanged:
+                            break;
+                        case EntityState.Added:
+                            currentData[propertyName] = property.CurrentValue;
+                            break;
+                        case EntityState.Deleted:
+                            previousData[propertyName] = originalValue;
+                            break;
+
+                        case EntityState.Modified:
+                            if (property.IsModified && originalValue?.Equals(property.CurrentValue) == false)
+                            {
+                                previousData[propertyName] = originalValue;
+                                currentData[propertyName] = property.CurrentValue;
+                            }
+
+                            break;
+                    }
+                }
+
+                string oldValues = previousData.Count == 0 ? null : _json.Serialize(previousData);
+                string newValues = currentData.Count == 0 ? null : _json.Serialize(currentData);
+                result.Add((entry, oldValues, newValues));
+            }
+
+            return result;
+        }
+
+        public override int SaveChanges()
+        {
+            var changes = OnBeforeSaveChanges();
+            return this.SaveChangeWithPublishEvents(_eventLogger, _mediator, changes, _json);
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            return SaveChanges();
+        }
+    }
+}
