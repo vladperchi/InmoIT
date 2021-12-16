d9d7714643bc91c56af21feb485327a113a29cc7
vladperchi <vladperchi@gmail.com>
2021-12-16 16:57:06 -0500

    Added Property JWT

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Settings/JwtSettings.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="JwtSettings.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Modules.Identity.Core.Settings
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; }
+
+        public int TokenExpirationInMinutes { get; set; }
+
+        public int RefreshTokenExpirationInDays { get; set; }
+    }
+}
