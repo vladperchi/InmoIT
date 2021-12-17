9d03432987c636e0e20321b527b58f0abd747768
vladperchi <vladperchi@gmail.com>
2021-12-17 02:04:16 -0500

    Added Default Password User Constant

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Constants/UserConstant.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UserConstant.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Modules.Identity.Core.Constants
+{
+    public static class UserConstant
+    {
+        public const string DefaultPassword = "@Qwerty123#";
+    }
+}
