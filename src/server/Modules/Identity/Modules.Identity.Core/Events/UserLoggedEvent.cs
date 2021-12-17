9c29fe47b6b32663e9ddd14ef25f7f10980192b7
vladperchi <vladperchi@gmail.com>
2021-12-17 02:10:32 -0500

    Added User Logged Event

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Events/UserLoggedEvent.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UserLoggedEvent.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using InmoIT.Modules.Identity.Core.Entities;
+using InmoIT.Shared.Core.Domain;
+
+namespace InmoIT.Modules.Identity.Core.Events
+{
+    public class UserLoggedEvent : Event
+    {
+        public Guid UserId { get; }
+
+        public new DateTime Timestamp { get; }
+
+        public UserLoggedEvent(Guid userId)
+        {
+            UserId = userId;
+            Timestamp = DateTime.Now;
+            AggregateId = userId;
+            RelatedEntities = new[] { typeof(InmoUser) };
+        }
+    }
+}
