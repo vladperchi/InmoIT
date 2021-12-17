7b4b4125124b2090b551ad05aa6c734b7ad3b3d7
vladperchi <vladperchi@gmail.com>
2021-12-17 02:34:06 -0500

    Added Dependency Injection Identity Core

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensions.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using FluentValidation;
+using InmoIT.Shared.Core.Extensions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InmoIT.Modules.Identity.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdentityCore(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddPaginatedFilterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
