222325bc9853428ed2e57868c24783a07bd14477
vladperchi <vladperchi@gmail.com>
2021-12-17 02:29:36 -0500

    Added Identity Custom Exception

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/IdentityCustomException.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IdentityCustomException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Extensions.Localization;
+using InmoIT.Shared.Core.Exceptions;
+
+namespace InmoIT.Modules.Identity.Core.Exceptions
+{
+    public class IdentityCustomException : CustomException
+    {
+        public IdentityCustomException(IStringLocalizer localizer, List<string> errors)
+            : base(localizer["Identity errors have occurred..."], errors, HttpStatusCode.InternalServerError)
+        {
+        }
+    }
+}
---
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/IdentityException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IdentityException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+
+namespace InmoIT.Modules.Identity.Core.Exceptions
+{
+    public class IdentityException : CustomException
+    {
+        public IdentityException(string message, List<string> errors = default, HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
+            : base(message, errors, statusCode)
+        {
+        }
+    }
+}
