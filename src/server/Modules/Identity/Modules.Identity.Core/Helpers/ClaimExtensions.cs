b812024d302af1becf84cae40530024664b4687e
vladperchi <vladperchi@gmail.com>
2021-12-17 02:44:51 -0500

    Added Claims Helper For Permissions

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Helpers/ClaimExtensions.cs
@@ -0,0 +1,75 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ClaimExtensions.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using InmoIT.Modules.Identity.Core.Entities;
+using InmoIT.Shared.Core.Constants;
+using InmoIT.Shared.Dtos.Identity.Roles;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace InmoIT.Modules.Identity.Core.Helpers
+{
+    public static class ClaimsHelper
+    {
+        public static void GetAllPermissions(this List<RoleClaimModel> allPermissions)
+        {
+            foreach (var module in typeof(PermissionsConstant).GetNestedTypes())
+            {
+                string moduleName = string.Empty;
+                string moduleDescription = string.Empty;
+
+                if (module.GetCustomAttributes(typeof(DisplayNameAttribute), true)
+                    .FirstOrDefault() is DisplayNameAttribute displayNameAttribute)
+                {
+                    moduleName = displayNameAttribute.DisplayName;
+                }
+
+                if (module.GetCustomAttributes(typeof(DescriptionAttribute), true)
+                    .FirstOrDefault() is DescriptionAttribute descriptionAttribute)
+                {
+                    moduleDescription = descriptionAttribute.Description;
+                }
+
+                foreach (var fi in module.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy))
+                {
+                    object propertyValue = fi.GetValue(null);
+
+                    if (propertyValue is not null)
+                    {
+                        allPermissions.Add(new() { Value = propertyValue.ToString(), Type = ClaimConstant.Permission, Group = moduleName, Description = moduleDescription });
+                    }
+                }
+            }
+        }
+
+        public static async Task<IdentityResult> AddPermissionClaimAsync(this RoleManager<InmoRole> roleManager, InmoRole role, string permission)
+        {
+            var allClaims = await roleManager.GetClaimsAsync(role);
+            if (!allClaims.Any(a => a.Type == ClaimConstant.Permission && a.Value == permission))
+            {
+                return await roleManager.AddClaimAsync(role, new(ClaimConstant.Permission, permission));
+            }
+
+            return IdentityResult.Failed();
+        }
+
+        public static async Task AddCustomPermissionClaimAsync(this RoleManager<InmoRole> roleManager, InmoRole role, string permission)
+        {
+            var allClaims = await roleManager.GetClaimsAsync(role);
+            if (!allClaims.Any(a => a.Type == ClaimConstant.Permission && a.Value == permission))
+            {
+                await roleManager.AddClaimAsync(role, new(ClaimConstant.Permission, permission));
+            }
+        }
+    }
+}
