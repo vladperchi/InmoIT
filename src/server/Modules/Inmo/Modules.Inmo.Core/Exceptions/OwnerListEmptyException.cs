b8f2ec8e60fec17fbb25aa7a06614b03922c35d5
vladperchi <vladperchi@gmail.com>
2021-12-29 11:21:42 -0500

    Added File

---
+++ src/server/Modules/Inmo/Modules.Inmo.Core/Exceptions/OwnerListEmptyException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="OwnerListEmptyException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Modules.Inmo.Core.Exceptions
+{
+    public class OwnerListEmptyException : CustomException
+    {
+        public OwnerListEmptyException(IStringLocalizer localizer)
+            : base(localizer["Owner List is empty..."], null, HttpStatusCode.NoContent)
+        {
+        }
+    }
+}
---
+++ src/server/Modules/Inmo/Modules.Inmo.Core/Exceptions/OwnerNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="OwnerNotFoundException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Modules.Inmo.Core.Exceptions
+{
+    public class OwnerNotFoundException : CustomException
+    {
+        public OwnerNotFoundException(IStringLocalizer localizer)
+            : base(localizer["Owner was not found..."], null, HttpStatusCode.NotFound)
+        {
+        }
+    }
+}
