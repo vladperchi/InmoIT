056039e54569c4225757573096b17e5bd9900abe
vladperchi <vladperchi@gmail.com>
2021-12-29 11:22:55 -0500

    Added File Owner Filter Specification

---
+++ src/server/Modules/Inmo/Modules.Inmo.Core/Specifications/OwnerFilterSpecification.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="OwnerFilterSpecification.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using InmoIT.Modules.Inmo.Core.Entities;
+using InmoIT.Shared.Core.Interfaces.Specifications;
+
+namespace InmoIT.Modules.Inmo.Core.Specifications
+{
+    public class OwnerFilterSpecification : Specification<Owner>
+    {
+        public OwnerFilterSpecification(string searchString)
+        {
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                Criteria = p => !string.IsNullOrWhiteSpace(p.Email) && (p.Name.Contains(searchString) || p.SurName.Contains(searchString) || p.Address.Contains(searchString) || p.PhoneNumber.Contains(searchString));
+            }
+            else
+            {
+                Criteria = p => !string.IsNullOrWhiteSpace(p.Email);
+            }
+        }
+    }
+}
