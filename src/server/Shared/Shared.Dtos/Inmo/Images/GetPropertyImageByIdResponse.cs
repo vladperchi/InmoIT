1158368c8a01403d38f0365eb108abe22f101b81
vladperchi <vladperchi@gmail.com>
2021-12-31 00:15:48 -0500

    Added File

---
+++ src/server/Shared/Shared.Dtos/Inmo/Images/GetPropertyImageByIdResponse.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="GetPropertyImageByIdResponse.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace InmoIT.Shared.Dtos.Inmo.Images
+{
+    public record GetPropertyImageByIdResponse(Guid Id, string ImageUrl, string Caption, bool Enabled, Guid PropertyId);
+}
---
+++ src/server/Shared/Shared.Dtos/Inmo/Images/PaginatedPropertyImageFilter.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="PaginatedPropertyImageFilter.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using InmoIT.Shared.Dtos.Filters;
+
+namespace InmoIT.Shared.Dtos.Inmo.Images
+{
+    public class PaginatedPropertyImageFilter : PaginatedFilter
+    {
+        public string SearchString { get; set; }
+
+        public Guid[] PropertyIds { get; set; }
+    }
+}
