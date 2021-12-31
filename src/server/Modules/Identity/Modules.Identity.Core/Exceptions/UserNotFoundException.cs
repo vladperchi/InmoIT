8b8d9a8fcf39b0505f450813a6e0ca1d8f6eb31b
vladperchi <vladperchi@gmail.com>
2021-12-31 00:14:08 -0500

    Added File

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UserNotFoundException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Modules.Identity.Core.Exceptions
+{
+    public class UserNotFoundException : CustomException
+    {
+        public UserNotFoundException(IStringLocalizer localizer)
+            : base(localizer["User was not found..."], null, HttpStatusCode.NotFound)
+        {
+        }
+    }
+}
---
+++ src/server/Modules/Person/Modules.Person.Core/Exceptions/CustomerAlreadyExistsException.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="CustomerAlreadyExistsException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Modules.Person.Core.Exceptions
+{
+    public class CustomerAlreadyExistsException : CustomException
+    {
+        public CustomerAlreadyExistsException(IStringLocalizer localizer)
+            : base(localizer["Customer already exists..."], null, HttpStatusCode.BadRequest)
+        {
+        }
+    }
+}
