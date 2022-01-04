9c90ec2b5272ca678ea89eb24f2b2a0416f47a51
vladperchi <vladperchi@gmail.com>
2022-01-03 23:48:35 -0500

    Added Exception Custom Owner

---
+++ src/server/Modules/Inmo/Modules.Inmo.Core/Exceptions/OwnerCustomException.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="OwnerCustomException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Extensions.Localization;
+using InmoIT.Shared.Core.Exceptions;
+
+namespace InmoIT.Modules.Inmo.Core.Exceptions
+{
+    public class OwnerCustomException : CustomException
+    {
+        public OwnerCustomException(IStringLocalizer localizer, List<string> errors)
+            : base(localizer["Owner errors have occurred..."], errors, HttpStatusCode.InternalServerError)
+        {
+        }
+    }
+}
