a88bd6d0520638a6dda6bbe30cb9f63232c9fdd7
vladperchi <vladperchi@gmail.com>
2022-01-05 02:42:02 -0500

    Added Constants Gender And Group For Customers And Owners

---
+++ src/server/Shared/Shared.Core/Constants/GenderConstant.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="GenderConstant.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.ComponentModel;
+
+namespace InmoIT.Shared.Core.Constants
+{
+    public static class GenderConstant
+    {
+        [DisplayName("GenderType")]
+        [Description("Gender Type Users")]
+        public static class GenderType
+        {
+            public const string Female = "Female";
+            public const string Male = "Male";
+        }
+    }
+}
