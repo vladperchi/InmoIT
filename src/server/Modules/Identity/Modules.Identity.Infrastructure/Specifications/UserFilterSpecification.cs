0b0a6bc3710a29338a3c48af55afff520910b19a
vladperchi <vladperchi@gmail.com>
2022-01-05 04:22:50 -0500

    Modified File

--- src/server/Modules/Identity/Modules.Identity.Infrastructure/Specifications/UserFilterSpecification.cs
+++ src/server/Modules/Identity/Modules.Identity.Infrastructure/Specifications/UserFilterSpecification.cs
@@ -17,7 +17,11 @@ namespace InmoIT.Modules.Identity.Infrastructure.Specifications
         {
             if (!string.IsNullOrEmpty(searchString))
             {
-                Criteria = p => p.FirstName.Contains(searchString) || p.LastName.Contains(searchString) || p.Email.Contains(searchString) || p.PhoneNumber.Contains(searchString) || p.UserName.Contains(searchString);
+                Criteria = p => p.FirstName.Contains(searchString)
+                || p.LastName.Contains(searchString)
+                || p.Email.Contains(searchString)
+                || p.PhoneNumber.Contains(searchString)
+                || p.UserName.Contains(searchString);
             }
             else
             {
