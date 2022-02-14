cf380679d92a76362d64116bf0115153d5bb4add
vladperchi <vladperchi@gmail.com>
2022-02-14 18:58:15 -0500

    Added Exception

---
+++ src/server/Shared/Shared.Core/Exceptions/EventLogCustomException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="EventLogCustomException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Shared.Core.Exceptions
+{
+    public class EventLogCustomException : CustomException
+    {
+        public EventLogCustomException(IStringLocalizer localizer, List<string> errors)
+            : base(localizer["Event Log errors have occurred..."], errors, HttpStatusCode.InternalServerError)
+        {
+        }
+    }
+}
