b12beddb4c683c29374a6d9768c1600b17ebfd72
vladperchi <vladperchi@gmail.com>
2022-02-14 18:57:55 -0500

    Added Exception

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/RolClaimListEmptyException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="RolClaimListEmptyException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Modules.Identity.Core.Exceptions
+{
+    public class RolClaimListEmptyException : CustomException
+    {
+        public RolClaimListEmptyException(IStringLocalizer localizer)
+            : base(localizer["Rol Claim list is empty..."], null, HttpStatusCode.NoContent)
+        {
+        }
+    }
+}
---
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/RolListEmptyException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="RolListEmptyException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Modules.Identity.Core.Exceptions
+{
+    public class RolListEmptyException : CustomException
+    {
+        public RolListEmptyException(IStringLocalizer localizer)
+            : base(localizer["Rol list is empty..."], null, HttpStatusCode.NoContent)
+        {
+        }
+    }
+}
