dff1a486e0a15ce785c8c28a554f4167ddfe2ab3
vladperchi <vladperchi@gmail.com>
2022-02-15 22:32:31 -0500

    Added Host Builder Extensions Serilog

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="HostBuilderExtensions.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace InmoIT.Shared.Infrastructure.Extensions
+{
+     internal static class HostBuilderExtensions
+    {
+        internal static IHostBuilder UseSerilog(this IHostBuilder builder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Development.json")
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+            SerilogHostBuilderExtensions.UseSerilog(builder);
+            return builder;
+        }
+    }
+}
