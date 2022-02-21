3d03d48efff511e243fb9d92a0d9b13cb6508245
vladperchi <vladperchi@gmail.com>
2022-02-21 16:27:43 -0500

    Added Domain Event User Picture

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Features/Users/Events/UserPictureUpdateEvent.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UserPictureUpdateEvent.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using InmoIT.Modules.Identity.Core.Entities;
+using InmoIT.Shared.Core.Domain;
+
+namespace InmoIT.Modules.Identity.Core.Features.Users.Events
+{
+    public class UserPictureUpdateEvent : Event
+    {
+        public string Id { get; }
+
+        public string ImageUrl { get; }
+
+        public UserPictureUpdateEvent(InmoUser user)
+        {
+            Id = user.Id;
+            ImageUrl = user.ImageUrl;
+            AggregateId = Guid.TryParse(user.Id, out var aggregateId)
+                ? aggregateId
+                : Guid.NewGuid();
+            RelatedEntities = new[] { typeof(InmoUser) };
+            EventDescription = "Updated User Picture.";
+        }
+    }
+}
