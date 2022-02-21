c045ae3d751b3f2a0e13fb72607582a09571be4c
vladperchi <vladperchi@gmail.com>
2022-02-21 11:41:13 -0500

    Added File

---
+++ src/server/Modules/Document/Modules.Document.Infrastructure/Services/DocumentService.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="DocumentService.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using InmoIT.Modules.Document.Core.Abstractions;
+using InmoIT.Shared.Core.Integration.Document;
+using Microsoft.EntityFrameworkCore;
+
+namespace InmoIT.Modules.Document.Infrastructure.Services
+{
+    public class DocumentService : IDocumentService
+    {
+        private readonly IDocumentDbContext _context;
+
+        public DocumentService(IDocumentDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> IsDocumentTypeUsed(Guid documentTypeId)
+        {
+            return await _context.Documents.AnyAsync(x => x.DocumentTypeId == documentTypeId);
+        }
+
+        public async Task<int> GetCountAsync()
+        {
+            return await _context.Documents.CountAsync();
+        }
+    }
+}
