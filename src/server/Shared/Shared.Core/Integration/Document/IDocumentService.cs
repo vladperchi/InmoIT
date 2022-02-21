624ea87854ea268438413d9750af1de3c41d1526
vladperchi <vladperchi@gmail.com>
2022-02-21 11:42:33 -0500

    Added File

---
+++ src/server/Shared/Shared.Core/Integration/Document/IDocumentService.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="IDocumentService.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using System;
+
+namespace InmoIT.Shared.Core.Integration.Document
+{
+    public interface IDocumentService
+    {
+        Task<bool> IsDocumentTypeUsed(Guid documentTypeId);
+
+        Task<int> GetCountAsync();
+    }
+}
---
+++ src/server/Shared/Shared.Dtos/Identity/Users/ChangePasswordRequest.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ChangePasswordRequest.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Dtos.Identity.Users
+{
+    public class ChangePasswordRequest
+    {
+        public string Password { get; set; }
+
+        public string NewPassword { get; set; }
+
+        public string ConfirmNewPassword { get; set; }
+    }
+}
