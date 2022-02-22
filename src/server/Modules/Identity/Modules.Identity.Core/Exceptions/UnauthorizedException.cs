2145d6c9003921ce5c7f73c82305cbf126e40518
vladperchi <vladperchi@gmail.com>
2022-02-22 01:24:28 -0500

    Added Unauthorized Exception

--- src/server/Modules/Identity/Modules.Identity.Core/Exceptions/UnauthorizedException.cs
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/UnauthorizedException.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="UnauthorizedException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
 
 namespace InmoIT.Modules.Identity.Core.Exceptions
 {
-    internal class UnauthorizedException
+    public class UnauthorizedException : CustomException
     {
+        public UnauthorizedException(string message, List<string> errors = default, HttpStatusCode statusCode = HttpStatusCode.Unauthorized)
+            : base(message, errors, statusCode)
+        {
+        }
     }
 }
