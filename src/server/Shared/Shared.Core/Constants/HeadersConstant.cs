6b4af2651db382e6162defa270c5b2eb59ba450b
vladperchi <vladperchi@gmail.com>
2022-02-23 18:19:01 -0500

    Added File Constants

---
+++ src/server/Shared/Shared.Core/Constants/HeadersConstant.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="HeadersConstant.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Core.Constants
+{
+    public static class HeadersConstant
+    {
+        public const string XFRAMEOPTIONS = "X-FrameOptions";
+        public const string XCONTENTTYPEOPTIONS = "X-Content-Type-Options";
+        public const string REFERRERPOLICY = "Referrer-Policy";
+        public const string PERMISSIONSPOLICY = "Permissions-Policy";
+        public const string SAMESITE = "SameSite";
+        public const string XXSSPROTECTION = "X-XSS-Protection";
+    }
+}
