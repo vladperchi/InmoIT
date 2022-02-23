aec0b1c355d0efc85d011d54f3c73e6452e2b56a
vladperchi <vladperchi@gmail.com>
2022-02-23 18:19:31 -0500

    Added File Settings

---
+++ src/server/Shared/Shared.Core/Settings/HeaderSettings.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="HeaderSettings.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Core.Settings
+{
+    public class HeaderSettings
+    {
+        public bool Enable { get; set; }
+
+        public string XFrameOptions { get; set; }
+
+        public string XContentTypeOptions { get; set; }
+
+        public string ReferrerPolicy { get; set; }
+
+        public string PermissionsPolicy { get; set; }
+
+        public string SameSite { get; set; }
+
+        public string XXSSProtection { get; set; }
+    }
+}
