1a8b03bdb81eae4609efd4846f2eee76ecf73cfe
vladperchi <vladperchi@gmail.com>
2022-02-23 18:18:23 -0500

    Added File Exception

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/ForbiddenException.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ForbiddenException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+
+namespace InmoIT.Modules.Identity.Core.Exceptions
+{
+    public class ForbiddenException : CustomException
+    {
+        public ForbiddenException(string message, List<string> errors = default, HttpStatusCode statusCode = HttpStatusCode.Forbidden)
+            : base(message, errors, statusCode)
+        {
+        }
+    }
+}
