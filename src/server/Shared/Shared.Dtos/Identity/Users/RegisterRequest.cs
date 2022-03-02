9b3c42e0dde95bfe5334a539c391df71837729cd
vladperchi <vladperchi@gmail.com>
2022-03-02 17:11:19 -0500

    Fix Request

--- src/server/Shared/Shared.Dtos/Identity/Users/RegisterRequest.cs
+++ src/server/Shared/Shared.Dtos/Identity/Users/RegisterRequest.cs
@@ -12,37 +12,22 @@ namespace InmoIT.Shared.Dtos.Identity.Users
 {
     public class RegisterRequest
     {
-        [Required]
-        [MinLength(10)]
         public string FirstName { get; set; }
 
-        [Required]
-        [MinLength(10)]
         public string LastName { get; set; }
 
-        [Required]
         public string Email { get; set; }
 
-        [Required]
-        [Compare("Email")]
         public bool EmailConfirmed { get; set; }
 
-        [Required]
-        [MinLength(8)]
         public string UserName { get; set; }
 
-        [Required]
-        [MinLength(8)]
         public string Password { get; set; }
 
-        [Required]
-        [Compare("Password")]
         public string ConfirmPassword { get; set; }
 
-        [Required]
         public string PhoneNumber { get; set; }
 
-        [Compare("PhoneNumber")]
         public bool PhoneNumberConfirmed { get; set; }
     }
 }
