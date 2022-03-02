478dd30818a2e2b2b96f1a259de7a392ec545815
vladperchi <vladperchi@gmail.com>
2022-03-02 17:15:57 -0500

    Added Token Request

---
+++ src/server/Shared/Shared.Dtos/Identity/Tokens/TokenUserRequest.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="TokenUserRequest.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Dtos.Identity.Tokens
+{
+    public record TokenUserRequest(string Email, string Password);
+}
