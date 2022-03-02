84235bd0ab82b6415ea92ba63194d873de372e8c
vladperchi <vladperchi@gmail.com>
2022-03-02 14:16:37 -0500

    Changed Code Identity Interface-Service => User Interface-Service

--- src/server/Modules/Identity/Modules.Identity.Core/Abstractions/IUserService.cs
+++ src/server/Modules/Identity/Modules.Identity.Core/Abstractions/IUserService.cs
@@ -19,15 +19,35 @@ namespace InmoIT.Modules.Identity.Core.Abstractions
 
         Task<IResult<UserResponse>> GetByIdAsync(string userId);
 
-        Task<IResult<UserRolesResponse>> GetRolesAsync(string userId);
+        Task<IResult<string>> GetPictureAsync(string userId);
+
+        Task<IResult<UserRolesResponse>> GetRolesByUserAsync(string userId);
+
+        Task<IResult> RegisterAsync(RegisterRequest request, string origin);
 
         Task<IResult<string>> UpdateAsync(UpdateUserRequest request);
 
-        Task<IResult<string>> UpdateUserRolesAsync(string userId, UserRolesRequest request);
+        Task<IResult<string>> UpdatePictureAsync(string userId, UpdateUserPictureRequest request);
+
+        Task<IResult<string>> UpdateRolesByUserAsync(string userId, UserRolesRequest request);
+
+        Task<IResult<string>> ConfirmEmailAsync(string userId, string code);
+
+        Task<IResult<string>> ConfirmPhoneNumberAsync(string userId, string code);
+
+        Task<IResult> ForgotPasswordAsync(ForgotPasswordRequest request, string origin);
+
+        Task<IResult> ResetPasswordAsync(ResetPasswordRequest request);
+
+        Task<IResult> ChangePasswordAsync(string userId, ChangePasswordRequest request);
+
+        Task<Result<string>> ExportAsync(string searchString = "");
 
         Task<Result<string>> DeleteAsync(string userId);
 
-        Task<Result<string>> ExportAsync(string searchString = "");
+        Task<bool> ExistsWithNameAsync(string name);
+
+        Task<bool> ExistsWithEmailAsync(string email, string exceptId = null);
 
         Task<bool> ExistsWithPhoneNumberAsync(string phoneNumber, string exceptId = null);
 
