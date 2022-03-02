162d62bf91998a51c05affe2e64b97e4b8efad14
vladperchi <vladperchi@gmail.com>
2022-03-02 17:16:17 -0500

    Added Token Response

---
+++ src/server/Shared/Shared.Dtos/Identity/Tokens/TokenUserResponse.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="TokenUserResponse.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace InmoIT.Shared.Dtos.Identity.Tokens
+{
+    public record TokenUserResponse(string Token, string RefreshToken, DateTime RefreshTokenExpiryTime);
+}
