86afd0b352ede7ad791b3fb8c8dd6713e1c2cbaf
vladperchi <vladperchi@gmail.com>
2022-03-10 13:07:18 -0500

    Added Strind Keys As Constants

---
+++ src/server/Shared/Shared.Core/Constants/StringKeys.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="StringKeys.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Core.Constants
+{
+    public static class StringKeys
+    {
+        public const string Code = "code";
+        public const string UserId = "userId";
+        public const string Token = "Token";
+    }
+}
