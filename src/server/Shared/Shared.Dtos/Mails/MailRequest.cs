abe6fa28d90cf2cc3bdaca2a2561f0167cbacb28
vladperchi <vladperchi@gmail.com>
2022-03-10 12:46:19 -0500

    Fix SMTP  Mail

--- src/server/Shared/Shared.Dtos/Mails/MailRequest.cs
+++ src/server/Shared/Shared.Dtos/Mails/MailRequest.cs
@@ -6,16 +6,47 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
+
 namespace InmoIT.Shared.Dtos.Mails
 {
     public class MailRequest
     {
-        public string To { get; set; }
+        public MailRequest(List<string> to, string subject, string body = null, string from = null, string displayName = null, string replyTo = null, string replyToName = null, List<string> bcc = null, List<string> cc = null, IDictionary<string, byte[]> attachmentData = null, IDictionary<string, string> headers = null)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+            From = from;
+            DisplayName = displayName;
+            ReplyTo = replyTo;
+            ReplyToName = replyToName;
+            Bcc = bcc ?? new List<string>();
+            Cc = cc ?? new List<string>();
+            AttachmentData = attachmentData ?? new Dictionary<string, byte[]>();
+            Headers = headers ?? new Dictionary<string, string>();
+        }
 
-        public string Subject { get; set; }
+        public List<string> To { get; }
 
-        public string Body { get; set; }
+        public string Subject { get; }
 
-        public string From { get; set; }
+        public string Body { get; }
+
+        public string From { get; }
+
+        public string DisplayName { get; }
+
+        public string ReplyTo { get; }
+
+        public string ReplyToName { get; }
+
+        public List<string> Bcc { get; }
+
+        public List<string> Cc { get; }
+
+        public IDictionary<string, byte[]> AttachmentData { get; }
+
+        public IDictionary<string, string> Headers { get; }
     }
 }
--- src/server/Shared/Shared.Infrastructure/Services/SmtpMailService.cs
+++ src/server/Shared/Shared.Infrastructure/Services/SmtpMailService.cs
@@ -7,27 +7,39 @@
 // --------------------------------------------------------------------------------------------------
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using InmoIT.Shared.Core.Interfaces.Services;
+using InmoIT.Shared.Core.Interfaces.Services.Identity;
 using InmoIT.Shared.Core.Settings;
+using InmoIT.Shared.Core.Wrapper;
 using InmoIT.Shared.Dtos.Mails;
 using MailKit.Net.Smtp;
 using MailKit.Security;
+
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MimeKit;
+
+using static InmoIT.Shared.Core.Constants.PermissionsConstant;
 
 namespace InmoIT.Shared.Infrastructure.Services
 {
     /// <inheritdoc cref = "IMailService" />
     public class SmtpMailService : IMailService
-    {
+{
         private readonly MailSettings _settings;
+        private readonly IStringLocalizer<SmtpMailService> _localizer;
         private readonly ILogger<SmtpMailService> _logger;
 
-        public SmtpMailService(IOptions<MailSettings> settings, ILogger<SmtpMailService> logger)
+        public SmtpMailService(
+            IOptions<MailSettings> settings,
+            IStringLocalizer<SmtpMailService> localizer,
+            ILogger<SmtpMailService> logger)
         {
             _settings = settings.Value;
+            _localizer = localizer;
             _logger = logger;
         }
 
@@ -35,26 +47,56 @@ namespace InmoIT.Shared.Infrastructure.Services
         {
             try
             {
-                var email = new MimeMessage
+                var email = new MimeMessage();
+
+                email.From.Add(new MailboxAddress(_settings.DisplayName, request.From ?? _settings.From));
+                foreach (string address in request.To)
+                    email.To.Add(MailboxAddress.Parse(address));
+
+                if (!string.IsNullOrEmpty(request.ReplyTo))
+                    email.ReplyTo.Add(new MailboxAddress(request.ReplyToName, request.ReplyTo));
+
+                if (request.Bcc != null)
                 {
-                    Sender = new MailboxAddress(_settings.DisplayName, request.From ?? _settings.From),
-                    Subject = request.Subject,
-                    Body = new BodyBuilder
-                    {
-                        HtmlBody = request.Body
-                    }.ToMessageBody()
-                };
-                email.To.Add(MailboxAddress.Parse(request.To));
+                    foreach (string address in request.Bcc.Where(bccValue => !string.IsNullOrWhiteSpace(bccValue)))
+                        email.Bcc.Add(MailboxAddress.Parse(address.Trim()));
+                }
+
+                if (request.Cc != null)
+                {
+                    foreach (string address in request.Cc.Where(ccValue => !string.IsNullOrWhiteSpace(ccValue)))
+                        email.Cc.Add(MailboxAddress.Parse(address.Trim()));
+                }
+
+                if (request.Headers != null)
+                {
+                    foreach (var header in request.Headers)
+                        email.Headers.Add(header.Key, header.Value);
+                }
+
+                var builder = new BodyBuilder();
+                email.Sender = new MailboxAddress(request.DisplayName ?? _settings.DisplayName, request.From ?? _settings.From);
+                email.Subject = request.Subject;
+                builder.HtmlBody = request.Body;
+
+                if (request.AttachmentData != null)
+                {
+                    foreach (var attachmentInfo in request.AttachmentData)
+                        builder.Attachments.Add(attachmentInfo.Key, attachmentInfo.Value);
+                }
+
+                email.Body = builder.ToMessageBody();
+
                 using var smtp = new SmtpClient();
                 await smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls);
                 await smtp.AuthenticateAsync(_settings.UserName, _settings.Password);
                 await smtp.SendAsync(email);
-                _logger.LogInformation($"Send email to {email.To} successfull");
+                _logger.LogInformation(string.Format(_localizer["Send email to {0} successfull"], email.To));
                 await smtp.DisconnectAsync(true);
             }
             catch (Exception ex)
             {
-                _logger.LogError($"An error occurred while send email to {request.To}", ex.Message);
+                _logger.LogError(string.Format(_localizer["An error occurred while send email to {0}. Error message: {1}"], request.To, ex.Message));
             }
         }
     }
