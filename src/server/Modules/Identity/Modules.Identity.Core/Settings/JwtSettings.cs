888c33396d4a719672a82709cff1496fe58f4347
vladperchi <vladperchi@gmail.com>
2022-03-10 12:48:27 -0500

    Removed Properties String

--- src/server/Modules/Identity/Modules.Identity.Core/Settings/JwtSettings.cs
+++ src/server/Modules/Identity/Modules.Identity.Core/Settings/JwtSettings.cs
@@ -12,10 +12,6 @@ namespace InmoIT.Modules.Identity.Core.Settings
     {
         public string Key { get; set; }
 
-        public string Issuer { get; set; }
-
-        public string Audience { get; set; }
-
         public int TokenExpirationInMinutes { get; set; }
 
         public int RefreshTokenExpirationInDays { get; set; }
