348b8c3c298fd74e00992ae13749b6f4c35d1906
vladperchi <vladperchi@gmail.com>
2022-03-21 21:36:54 -0500

    Created ReservedCustomException

---
+++ src/server/Modules/Identity/Modules.Identity.Core/Exceptions/ReservedCustomException.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ReservedCustomException.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using InmoIT.Shared.Core.Exceptions;
+
+using Microsoft.Extensions.Localization;
+
+namespace InmoIT.Modules.Identity.Core.Exceptions
+{
+    public class ReservedCustomException : CustomException
+    {
+        public ReservedCustomException(IStringLocalizer localizer, List<string> errors)
+            : base(localizer["Method reserved for in-scope initialization"], errors, HttpStatusCode.MethodNotAllowed)
+        {
+        }
+    }
+}
