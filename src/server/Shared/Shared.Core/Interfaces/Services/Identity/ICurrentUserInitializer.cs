96331041a4640a5f09e0bcc7217499e9abce4fd6
vladperchi <vladperchi@gmail.com>
2022-03-21 21:38:34 -0500

    Created ICurrentUserInitializer

---
+++ src/server/Shared/Shared.Core/Interfaces/Services/Identity/ICurrentUserInitializer.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="ICurrentUserInitializer.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace InmoIT.Shared.Core.Interfaces.Services.Identity
+{
+    public interface ICurrentUserInitializer
+    {
+        void SetCurrentUserId(string userId);
+    }
+}
