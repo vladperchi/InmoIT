f823580c81a5a6d4be4198a7ff901ac35b71f5f7
vladperchi <vladperchi@gmail.com>
2022-03-21 20:58:49 -0500

    Created MiddlewareExtensions

---
+++ src/server/Shared/Shared.Infrastructure/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------
+// <copyright file="MiddlewareExtensions.cs" company="InmoIT">
+// Copyright (c) InmoIT. All rights reserved.
+// Developer: Vladimir P. CHibás (vladperchi).
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------
+
+using InmoIT.Shared.Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InmoIT.Shared.Infrastructure.Extensions
+{
+    internal static class MiddlewareExtensions
+    {
+        internal static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder app) => app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+        internal static IServiceCollection AddExceptionMiddleware(this IServiceCollection services) => services.AddScoped<ExceptionHandlerMiddleware>();
+    }
+}
